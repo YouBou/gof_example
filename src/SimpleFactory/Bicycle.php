0804c7632f8324fefe490c67205ba91f96c706ba
YouBou <33052528+YouBou@users.noreply.github.com>
2021-05-28 15:04:19 +0900

    [feat] SimpleFactory sample src

---
+++ src/SimpleFactory/Bicycle.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace DesignPattern\SimpleFactory;
+
+/**
+ * Class Bicycle
+ * @package DesignPattern\SimpleFactory
+ */
+class Bicycle
+{
+    /**
+     * @param string $destination
+     * @return void
+     */
+    public function driveTo(string $destination): void
+    {
+    }
+}
