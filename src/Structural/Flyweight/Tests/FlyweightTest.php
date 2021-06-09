0441787534c881113c1c417abf5632d2c0cf9ff8
YouBou <33052528+YouBou@users.noreply.github.com>
2021-06-09 09:56:32 +0900

    [feat] Flyweight Test

---
+++ src/Structural/Flyweight/Tests/FlyweightTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace DesignPattern\Structural\Flyweight\Tests;
+
+use DesignPattern\Structural\Flyweight\TextFactory;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class FlyweightTest
+ * @package DesignPattern\Structural\Flyweight\Tests
+ */
+class FlyweightTest extends TestCase
+{
+    private array $characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+        'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+    private array $fonts = ['Arial', 'Times New Roman', 'Verdana', 'Helvetica'];
+
+    public function testFlyweight(): void
+    {
+        $factory = new TextFactory();
+
+        for ($i = 0; $i <= 10; $i++) {
+            foreach ($this->characters as $char) {
+                foreach ($this->fonts as $font) {
+                    $flyweight = $factory->get($char);
+                    $rendered = $flyweight->render($font);
+
+                    self::assertSame(sprintf('Character %s with font %s', $char, $font), $rendered);
+                }
+            }
+        }
+
+        foreach ($this->fonts as $word) {
+            $flyweight = $factory->get($word);
+            $rendered = $flyweight->render('foobar');
+
+            self::assertSame(sprintf('Word %s with font foobar', $word), $rendered);
+        }
+
+        self::assertCount(count($this->characters) + count($this->fonts), $factory);
+    }
+}
